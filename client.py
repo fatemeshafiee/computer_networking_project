72fcecf1e87cdd6fe29be7e6873932137950f9ec
Fateme Shafiee <61053360+fatemeshafiee@users.noreply.github.com>
2022-06-16 18:33:27 +0430

    save sudent info, avg, max_avg, min_avg

--- client.py
+++ client.py
@@ -1,11 +1,19 @@
 import socket
 import threading
+import json
+
 DISCONNECT_MSG = "!Disconnected"
+STUDENT_INFO="Student information"
 SERVER = "192.168.1.6"
 PORT = 4500
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((SERVER, PORT))
+
+
+
 
 def send_msg(msg):
 	message = msg.encode('utf-8')
@@ -13,8 +21,36 @@ def send_msg(msg):
 	send_legnth = str(msg_legnth).encode('utf-8')
 	send_legnth += b' ' * (64 - len(send_legnth))
 	client.send(send_legnth)
-	client.send(message )
+	client.send(message)
 	print(client.recv(2048).decode('utf-8'))
+
+def get_student_info():
+	information ={}
+	print("Please Enter the Student first name:")
+	information['name'] = input()
+	print("Please Enter the Student last name:")
+	information['last_name'] = input()
+	print("Please Enter the Student national code:")
+	information['national code'] = input()
+	print("Please Enter the Student ssn")
+	information['SSN'] = input()
+	marks = []
+	for i in range(5):
+		print("Please Enter the Student mark:")
+		m = int(input())
+		marks.append(m)
+	information['marks'] = marks
+	return information
+
+def send_students_info():
+	informations = {}
+	#for i in range(1,6):
+	for i in range(1):
+		print("Enter the Student " + str(i)+" information:")
+		informations[str(i)] = get_student_info()
+	informations = json.dumps(informations)
+	send_msg(STUDENT_INFO)
+	send_msg(informations)
 
 connected = True
 while connected:
@@ -22,4 +58,12 @@ while connected:
 	input_string = input()
 	if input_string == DISCONNECT_MSG:
 		connected = False
-	send_msg(input_string)
+		send_msg(DISCONNECT_MSG)
+	elif input_string == STUDENT_INFO:
+		send_students_info()
+	elif input_string == 'Average':
+		send_msg('Average')
+	elif input_string == 'Sort':
+		send_msg('Average')
+
+	#send_msg(input_string)
--- server.py
+++ server.py
@@ -1,20 +1,21 @@
 import socket
 import threading
+import json
 
 Header = 64
+NUM_STU = 1
 PORT = 4500
 DISCONNECT_MSG = "!Disconnected"
+STUDENT_INFO="Student information"
 SERVER = socket.gethostbyname(socket.gethostname()) #local ipv add
 
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 #socket.AF_INET  >> what type of add acc or loking for
 server.bind((SERVER, PORT))
 
-
-def handle_client(conn, addr):
-
-	print(f"[NEW CONNECTION ]{addr} connected.")
-
+	
+def save_student_info(conn, addr): 
+	conn.send("Saving student info ....".encode('utf-8'))
 	connected = True
 	while connected:
 		msg_legnth = conn.recv(Header).decode('utf-8')
@@ -22,17 +23,81 @@ def handle_client(conn, addr):
 
 			msg_legnth = int(msg_legnth)
 			msg = conn.recv(msg_legnth).decode('utf-8')
+			print(msg)
+			connected = False
+			with open('json_data'+str(addr)+'.json', 'w') as outfile:
+				outfile.write(msg)
+			conn.send("informations saved.".encode('utf-8'))
+			return json.loads(msg)
+def students_avg(informations):
+	ave = {}
+	for i in range(NUM_STU):
+
+		ave[informations[str(i)]['national code']] = sum(informations[str(i)]['marks']) / 5
+		ave = json.dumps(ave)
+	conn.send(ave.encode('utf-8'))
+
+
+def sort_avg(informations):
+	sorted_avg = {}
+	for i in range(NUM_STU):
+
+		sorted_avg[informations[str(i)]['SSN']] = sum(informations[str(i)]['marks']) / 5
+		sorted_avg = sorted(sorted_avg.items(), key = lambda kv: kv[1])
+		sorted_avg = json.dumps(sorted_avg)
+
+	conn.send(sorted_avg.encode('utf-8'))
+def Max_avg(informations):
+
+
+def Min_avg(informations):
+
+def handle_client(conn, addr):
+
+	print(f"[NEW CONNECTION ]{addr} connected.")
+
+	connected = True
+	informations = {}
+
+	while connected:
+		msg_legnth = conn.recv(Header).decode('utf-8')
+		
+		if len(msg_legnth):
+
+			msg_legnth = int(msg_legnth)
+			msg = conn.recv(msg_legnth).decode('utf-8')
 			print(f"[{addr}]{msg}")
-
-			if msg == DISCONNECT_MSG:
-				connected = False
-			conn.send("ACK".encode('utf-8'))
-				
-	conn.close()
-	print(f"[{addr} Connection Closed. ACTIVE CONNECTIONS]   {threading. active_count() - 2}")
-
-
+			if msg == STUDENT_INFO:
+				informations = save_student_info(conn, addr)
 	
+			elif msg == 'Average':
+
+				students_avg(informations)
+
+			elif msg=='Sort':
+
+				sort_avg(informations)
+
+			elif msg=="Max":
+
+				Max_avg(informations)
+
+			elif msg=="Min":
+				
+				Min_avg(informations)
+				
+			else:
+				connected = False
+				conn.send("!Disconnected".encode('utf-8'))
+				print(f"[{addr} Connection Closed. ACTIVE CONNECTIONS]  \
+				{threading. active_count() - 2}")
+
+
+			
+
+	conn.close()
+
+
 
 
 def start_server():
